ce4ba0d2ccf22f8d9e41ce5c8baa1e4f337ff463
Carambis <lepeshko1997@mail.ru>
2019-03-09 15:00:21 +0300

    Initial commit

---
+++ task-service/src/main/java/by/bsuir/controller/Controller.java
@@ -0,0 +1,13 @@
+package by.bsuir.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(value = "/get")
+    public String get(){
+        return "Success";
+    }
+}
