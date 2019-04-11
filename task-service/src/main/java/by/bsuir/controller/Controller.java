5a52cb4064e37c0302dabfa9c4f8bd6f23f0dd3b
Carambis <lepeshko1997@mail.ru>
2019-04-11 22:16:32 +0300

    add authenticated check

--- task-service/src/main/java/by/bsuir/controller/Controller.java
+++ task-service/src/main/java/by/bsuir/controller/Controller.java
@@ -3,6 +3,8 @@ package by.bsuir.controller;
 import by.bsuir.entity.Task;
 import by.bsuir.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,16 +22,19 @@ public class Controller {
         return "Success";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping(value = "/getTask")
     public Task getTask(@RequestParam("id") String id){
         return taskService.getTask(id);
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/getNextTask")
     public String getNextTask(){
         return "someTask";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/startTest")
     public void startTest(){
         taskService.startTest();
